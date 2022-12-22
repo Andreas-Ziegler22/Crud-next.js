bb4355fc0be2cc945c65630944742133afe63d08
MrBurp <andreass.ziegler@gmail.com>
2022-12-22 12:15:45 +0100

    home background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,13 @@ export default function Home() {
   return (
     <div
       className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-purple-500 to-blue-500
+      text-white
+
     `}
     >
-      <span className="text-4xl">Texto</span>
+      <h1>App</h1>
     </div>
   );
 }
