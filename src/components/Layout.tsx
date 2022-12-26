246d6d444bb70f77ae776b32916fab84ad8e2278
MrBurp <andreass.ziegler@gmail.com>
2022-12-26 21:48:18 +0100

    deleted 2 components

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,12 @@ interface LayoutProps {
 
 export default function Layout(props: LayoutProps) {
   return (
-    <div className={`flex flex-col w-2/3 bg-white text-gray-800 rounded-md`}>
+    <div
+      className={`
+          flex flex-col w-2/3
+          bg-white text-gray-800 rounded-md
+      `}
+    >
       <HeadTitle>{props.title}</HeadTitle>
       <div className="p-6">{props.children}</div>
     </div>
