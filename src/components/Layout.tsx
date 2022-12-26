b159dd81f96fb9eb4a7d8c0fa0c350d6135838ee
MrBurp <andreass.ziegler@gmail.com>
2022-12-26 16:49:42 +0100

    layout and title styled done

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,9 +11,11 @@ export default function Layout(props: LayoutProps) {
       className={`
     flex flex-col w-2/3
     bg-white text-gray-800
+    rounded-md
     `}
     >
       <Title>{props.title}</Title>
+      <div className="p-6">{props.children}</div>
     </div>
   );
 }
