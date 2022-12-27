2a23dfe9eda33540f150f3830096e22bfb170896
MrBurp <andreass.ziegler@gmail.com>
2022-12-27 08:30:39 +0100

    component table updated

--- src/components/TableForm.tsx
+++ src/components/TableForm.tsx
@@ -8,26 +8,31 @@ export default function TableForm(props: TableFormProps) {
   function renderTHEad() {
     return (
       <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Age</th>
+        <th className="text-left p-4">ID</th>
+        <th className="text-left p-4">Name</th>
+        <th className="text-left p-4">Age</th>
       </tr>
     );
   }
   function renderDataTBody() {
     return props.clients?.map((client, i) => {
       return (
-        <tr key={client.id}>
-          <td>{client.id}</td>
-          <td>{client.name}</td>
-          <td>{client.age}</td>
+        <tr
+          key={client.id}
+          className={`${i % 2 === 0 ? "bg-purple-200" : "bg-purple-100"}`}
+        >
+          <td className="text-left p-4">{client.id}</td>
+          <td className="text-left p-4">{client.name}</td>
+          <td className="text-left p-4">{client.age}</td>
         </tr>
       );
     });
   }
   return (
-    <table>
-      <thead className={`bg-gradient-to-r from-purple-500 to-purple-800`}>
+    <table className="w-full rounded-xl overflow-hidden">
+      <thead
+        className={`bg-gradient-to-r from-purple-500 to-purple-800 text-gray-100`}
+      >
         {renderTHEad()}
       </thead>
       <tbody>{renderDataTBody()}</tbody>
