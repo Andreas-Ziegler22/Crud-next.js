875201fe7a900e01f7024cb25c947faafb8d27fb
MrBurp <andreass.ziegler@gmail.com>
2022-12-28 11:04:15 +0100

    update interfaceFormProps

--- src/components/InterfaceForm.tsx
+++ src/components/InterfaceForm.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
+import Client from "./core/Client";
 import InputForm from "./InputForm";
 
-interface interfaceFormProps {}
+interface interfaceFormProps {
+  cliente: Client;
+}
 
 export default function InterfaceForm(props: interfaceFormProps) {
+  const [name, setName] = useState();
   return (
     <div>
-      <InputForm text="name" value="Test" />
+      <InputForm text="Name:" value="Test" />
+      <InputForm text="Age:" type="number" value="Test" />
     </div>
   );
 }
