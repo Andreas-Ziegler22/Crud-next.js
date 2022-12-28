7f560ff46be5236ff0f430cef065917675c08049
MrBurp <andreass.ziegler@gmail.com>
2022-12-28 09:30:20 +0100

    2 new components form and input

---
+++ src/components/InputForm.tsx
@@ -0,0 +1,5 @@
+interface InputFormProps {}
+
+export default function InputForm(props: InputFormProps) {
+  return <div></div>;
+}
---
+++ src/components/InterfaceForm.tsx
@@ -0,0 +1,11 @@
+import InputForm from "./InputForm";
+
+interface interfaceFormProps {}
+
+export default function InterfaceForm(props: interfaceFormProps) {
+  return (
+    <div>
+      <InputForm />
+    </div>
+  );
+}
