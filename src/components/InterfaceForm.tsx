0f61d6ad167a56cd4860949105568befbbae0c45
MrBurp <andreass.ziegler@gmail.com>
2022-12-28 11:15:23 +0100

    input id,name,age

--- src/components/InterfaceForm.tsx
+++ src/components/InterfaceForm.tsx
@@ -3,15 +3,18 @@ import Client from "./core/Client";
 import InputForm from "./InputForm";
 
 interface interfaceFormProps {
-  cliente: Client;
+  client: Client;
 }
 
 export default function InterfaceForm(props: interfaceFormProps) {
-  const [name, setName] = useState();
+  const id = props.client?.id;
+  const [name, setName] = useState(props.client?.name ?? "");
+  const [age, setAge] = useState(props.client?.name ?? 0);
   return (
     <div>
-      <InputForm text="Name:" value="Test" />
-      <InputForm text="Age:" type="number" value="Test" />
+      {id ? <InputForm onlyread text="ID" value={id} /> : false}
+      <InputForm text="Name:" value={name} />
+      <InputForm text="Age:" type="number" value={age} />
     </div>
   );
 }
